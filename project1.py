b08d0354edc2762781f119aa3334b4626c87896d
reddyb <databreddy@gmail.com>
2023-12-03 17:18:03 +0200

    Updates

--- project1.py
+++ project1.py
@@ -1,6 +1,9 @@
-# Version: 1.0
+# solved on 3 Dec 2023
+
+# Version: 1.1
 # Author: Bevlin Reddy / bevlinrdut@gmail.com
 # Project Euler: problem 1 - multiples of 3 and 5
+# URL: https://projecteuler.net/problem=1
 # Description: find the sum of all multiples of 3 and 5 less than 1000
 # Creation Date: 02-Dec-23
 
@@ -11,5 +14,5 @@ for i in range(0,max_num):
     if i % 3 == 0 or i % 5 == 0:
         sum += i
 
-print(sum)
+print("Sum = " + str(sum))
 
